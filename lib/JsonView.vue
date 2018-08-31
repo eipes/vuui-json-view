4ecf3ac22ce9567c64490a8259d826633341bbcd
eipes <eipes@sina.com>
2018-08-31 17:50:16 +0800

    init

---
+++ lib/JsonView.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="page-content">
+        <section class="content-section">
+            <div class="section-heading"><h1><em>DEMO</em> / json-view</h1></div>
+            <div class="main text-left">
+                <div class="left">
+                    <div class="title margin-bottom-20">JSON String</div>
+                    <textarea class="form-control" cols="30" rows="10" v-model="json"></textarea>
+                </div>
+                <div class="right">
+                    <div class="title margin-bottom-20">JSON View</div>
+                    <json-view :json="jsonObject"></json-view>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style scoped>
+    .main {
+        display: flex;
+    }
+
+    .left {
+        flex: 1;
+        padding: 20px;
+    }
+
+    .left textarea {
+        border: 1px solid #bfbfbf;
+        background-color: transparent;
+        border-radius: 0;
+    }
+
+    .right {
+        flex: 1;
+        padding: 20px;
+    }
+</style>
+
+<script>
+import JsonView from '@/components/JsonView.vue'
+
+export default {
+    computed: {
+        jsonObject () {
+            let ret = ''
+            try {
+                ret = JSON.parse(this.json)
+            } catch (e) {
+                ret = ''
+            }
+            return ret
+        }
+    },
+    data () {
+        return {
+            json: ''
+        }
+    },
+    components: {
+        JsonView
+    }
+}
+</script>
---
+++ readme.md
@@ -0,0 +1,22 @@
+# vue-json-view
+
+## Installation
+
+```shell
+npm install vue-json-view
+```
+
+## Usage
+```javascript
+import JsonView from "vue-json-view"
+```
+
+
+```html
+<div>
+  <json-view :json="jsonSource"></json-view>
+</div>
+```
+
+## Changelog
+* 1.0.0: Initial Release
